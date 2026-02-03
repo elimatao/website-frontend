ce3e8a59e57ecc6292e97355f8705bed119e2ec8
Elia Doumerc <eli.doumerc@gmail.com>
2026-02-03 22:59:16 +0000

    fix international routing in cs page and in articlecards

--- website-frontend/src/components/ArticleCard.tsx
+++ website-frontend/src/components/ArticleCard.tsx
@@ -6,8 +6,14 @@ import { useTranslations } from "next-intl";
 
 export default function ArticleCard({ articleData, route }: { articleData: ArticleMetadata, route: string }) {
     const t = useTranslations('Home');
+    console.log("route:", route);
+    console.log("article name", articleData.title, "locale:", articleData.locale);
     return (
-        <Link href={`${route}/${articleData.slug}`} locale={articleData.locale} className="block break-inside-avoid h-full">
+        <Link 
+            href={`/${route}/${articleData.slug}`} 
+            locale={articleData.locale} 
+            className="block break-inside-avoid h-full"
+        >
             <Card className="border hover:shadow-lg  transition-shadow hover:cursor-pointer h-full">
                 <CardHeader className="p-0 gap-0 border-0 border-b-1 border-accent rounded-t-xl overflow-hidden">
                     {articleData.featured_image && (
