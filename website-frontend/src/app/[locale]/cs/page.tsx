36bbe032e95ee35096200f71afe45d2e3480dff3
Elia Doumerc <eli.doumerc@gmail.com>
2026-02-05 11:43:56 +0000

    Improve page metadata.

--- website-frontend/src/app/[locale]/cs/page.tsx
+++ website-frontend/src/app/[locale]/cs/page.tsx
@@ -10,7 +10,7 @@ export default async function CSOverviewPage({params}: {params: Promise<{locale:
     const t = await getTranslations('CsListPage')
 
     return (
-        <div className="max-w-6xl mx-auto px-8">
+        <div className="max-w-6xl mx-auto px-8 pb-6">
             <h1 className="text-center text-4xl font-bold my-16">{t('title')}</h1>
             <ArticleListTemplate route="cs" locale={locale} />
         </div>
