ee0d2e6703ad384af8842002f7ed79e3a760ee50
Elia Doumerc <eli.doumerc@gmail.com>
2026-03-05 20:09:13 +0100

    Update legal documents

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,8 +10,8 @@ export default async function Footer() {
         <footer className="text-center p-4 grid grid-cols-2 md:grid-cols-4 gap-2 mt-auto border-t text-sm">
             <Link href="/cs/about_page">{t("about_page")}</Link>
             <Link locale="en" href="/cs/changelog">{t("changelog")}</Link>
-            <Link href="/legal" locale="es">{t("legal")}</Link>
-            <Link href="/privacy" locale="es">{t("privacy")}</Link>
+            <Link href="/legal">{t("legal")}</Link>
+            <Link href="/privacy">{t("privacy")}</Link>
         </footer>
     );
 }
